b1ab552a3e0d8bdb2a4237440cb5c95af5ba024c
Nikolas Pontikos <n.pontikos@gmail.com>
2015-09-19 14:51:17 +0100

    Update edges.py

--- edges.py
+++ edges.py
@@ -5,8 +5,10 @@ from skimage.io import imread
 from skimage.filters import roberts, sobel, scharr, prewitt
 
 image = imread('images/Figure-41.jpg')
+
 #green channel
 image=image[:,:,1]
+
 #edges = roberts(image)
 edges = sobel(image)
 
@@ -21,5 +23,16 @@ plt.show()
 plt.imshow(edges)
 plt.show()
 
+# probably need to threshold for clustering...
+
+# do clustering
+from sklearn import cluster
+from sklearn.neighbors import kneighbors_graph
+X = StandardScaler().fit_transform(X)
+connectivity = kneighbors_graph(X, n_neighbors=10, include_self=False)
+connectivity = 0.5 * (connectivity + connectivity.T)
+clustering = cluster.AgglomerativeClustering(linkage="average", affinity="cityblock", connectivity=connectivity, n_clusters=5000)
+clustering.fit(X)
+np.savetxt('clusters.csv',ward.labels_.astype(np.int))
 
 
