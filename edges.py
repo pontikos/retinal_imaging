75260d3e1054bccddd69374a0fad4f73da314a2a
Nikolas Pontikos <n.pontikos@gmail.com>
2015-09-19 14:23:51 +0100

    Update edges.py

--- edges.py
+++ edges.py
@@ -5,11 +5,23 @@ import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage.filters import roberts, sobel, scharr, prewitt
 
-image = imread('images/Figure_41.jpg')
+
+image = imread('images/Figure-41.jpg')
 #green channel
 image=image[:,:,1]
 #edges = roberts(image)
 edges = sobel(image)
+
+# needs Qt
+#from skimage.viewer import ImageViewer
+#viewer = ImageViewer(image)
+#viewer.show()
+
+plt.imshow(image)
+plt.show()
+
+plt.imshow(edges)
+plt.show()
 
 
 
