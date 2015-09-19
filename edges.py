adf69fdfb154f5ef9f5ab2c83d1de0761920bd1a
Nikolas Pontikos <n.pontikos@gmail.com>
2015-09-19 14:14:56 +0100

    Update edges.py

--- edges.py
+++ edges.py
@@ -6,8 +6,10 @@ from skimage.io import imread
 from skimage.filters import roberts, sobel, scharr, prewitt
 
 image = imread('images/Figure_41.jpg')
-
-edges = roberts(image)
+#green channel
+image=image[:,:,1]
+#edges = roberts(image)
 edges = sobel(image)
+
 
 
