061e94aa32ee5676f2d31ac4a9cdc40c3dbe4edf
Nikolas Pontikos <n.pontikos@gmail.com>
2015-09-19 13:40:51 +0100

    Update edges.py

--- edges.py
+++ edges.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage.filters import roberts, sobel, scharr, prewitt
 
-image = imread('Figure_41.jpg')
+image = imread('images/Figure_41.jpg')
 
 edges = roberts(image)
 edges = sobel(image)
